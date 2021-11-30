4ba83977bcf826b65a64ab3a9008c093cdc8a54e
SherlockVARM <anmoldhiman7111@gmail.com>
2021-11-30 09:06:06 +0530

    adding the code for oops

--- oops/assignment_operator.cpp
+++ oops/assignment_operator.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 using namespace std;
-class A
-{
-   
-    A &operator=(const A &t)
-    {
-        cout << "aur bhi ladlae kae haal h " << endl;
-        return *this;
-    }
-};
 int main()
 {
-    A t1, t2;
-
-    t2 = t1;
+    
     return 0;
 }
